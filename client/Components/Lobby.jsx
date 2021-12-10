4adcadd857999d51c4a38d3d6a31df6816b9a2ed
unknown <shaunstruwig.ss@gmail.com>
2021-12-10 14:32:06 +0100

    :styling: improvements in styling to the lobby page

--- client/Components/Lobby.jsx
+++ client/Components/Lobby.jsx
@@ -1,6 +1,8 @@
 import React,{useState} from 'react';
 import { useSelector, useDispatch,connect} from 'react-redux'
 import { ioPlayerIsReady } from '../reducers';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faUser } from '@fortawesome/free-solid-svg-icons';
 
 const Lobby = (props) => {
 
@@ -18,7 +20,7 @@ const Lobby = (props) => {
         }
     }
 
-    const appClass="w-full align-center content-center p-10 bg-spy";
+    const appClass="w-full align-center content-center p-10 bg-spy flex flex-col justify-between";
     const startbuttonClass = "bg-blue text-white w-36 p-2 rounded-lg hover:bg-lightblue transition duration-500 ease-in-out";
     const startButtonClassUnable = "bg-lightgray text-white w-36 p-2 rounded-lg transition duration-500 ease-in-out";
     const readybuttonClass = `bg-blue text-white  w-36 p-2 rounded-lg hover:bg-lightblue transition duration-500 ease-in-out ${props.selfAlias === 'Player1' ? 'mt-2' : ''}`;
@@ -29,7 +31,7 @@ const Lobby = (props) => {
             <div className="flex flex-row flex-wrap justify-between">
                 <div>
                     <h2 className="text-white text-2xl">Roomcode</h2>
-                    <h2 className="text-white text-3xl">{props.roomCode}</h2>
+                    <h2 className="text-white text-3xl animate-bounce text-center">{props.roomCode}</h2>
                 </div>
                 <div className="flex flex-col">
                     {props.selfAlias === 'Player1' ? <button className={props.canStartGame ? startbuttonClass : startButtonClassUnable} onClick={startGameHandler}>Start Game</button> : null}
@@ -39,12 +41,13 @@ const Lobby = (props) => {
                     }
                 </div>
             </div>
-            <ul className="flex flex-row">
+            <ul className="flex flex-row justify-evenly">
                 {
                 (props.players || []).map(player => {
-                    return <div key={player.playerID}>
+                    return <div key={player.playerID} className='flex flex-col content-center items-center'>
+                            <FontAwesomeIcon icon={faUser} className='text-white text-6xl'/>
                             <h2 className="text-white">{player.selfAlias}</h2>
-                            <p className="text-white">{player.readyToStart ? 'ready' : 'waiting...'}</p>   
+                            {player.readyToStart ? <p className="text-green align-center">ready</p> :<p className="text-white align-center animate-pulse">waiting</p>}   
                         </div>
                 })
                 }
