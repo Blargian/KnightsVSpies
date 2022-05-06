c60038b113a1535b15421fe61372fe534c836584
unknown <shaunstruwig.ss@gmail.com>
2022-05-06 13:56:19 +0200

    :bug: remove images from RoundWInner

--- client/Components/RoundWinner.jsx
+++ client/Components/RoundWinner.jsx
@@ -1,12 +1,10 @@
 import React from 'react';
-import knightsWonImage from '';
-import spiesWonImage from '';
 
 const RoundWinner = ({knightsWon}) => {
     return (
         <div className="text-center animate-fadein">
             <h1 className="text-white text-3xl">{knightsWon ? "Knights" : "Spies"} won the round</h1>
-            {knightsWon ? <img src={knightsWonImage}/> : <img src={spiesWonImage}/>}
+            {knightsWon ? <img src=""/> : <img src=""/>}
         </div>
     )
 }
